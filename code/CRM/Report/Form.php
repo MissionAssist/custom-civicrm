2c240cf5730740613cfc09a72571cfd364cdaf28
Stephen Palmstrom <stephen.palmstrom@missionassist.org.uk>
2025-08-07 19:44:40 +0100

    v6.5.0

--- code/CRM/Report/Form.php
+++ code/CRM/Report/Form.php
@@ -4930,7 +4930,7 @@ LEFT JOIN civicrm_contact {$field['alias']} ON {$field['alias']}.id = {$this->_a
       ],
       'is_deceased' => [],
       'deceased_date' => [
-        'title' => ts('Deceased Date'),
+        'title' => ts('Deceased / Closed Date'),
       ],
       'job_title' => [
         'title' => ts('Contact Job title'),
@@ -5003,7 +5003,7 @@ LEFT JOIN civicrm_contact {$field['alias']} ON {$field['alias']}.id = {$this->_a
         'type' => CRM_Utils_Type::T_DATE,
       ],
       'is_deceased' => [
-        'title' => ts('Deceased'),
+        'title' => ts('Deceased / Closed'),
         'type' => CRM_Utils_Type::T_BOOLEAN,
         'default' => $defaults['deceased'] ?? 0,
       ],
@@ -5622,7 +5622,7 @@ LEFT JOIN civicrm_contact {$field['alias']} ON {$field['alias']}.id = {$this->_a
         'is_fields' => TRUE,
       ],
       $options['prefix'] . 'is_deceased' => [
-        'title' => $options['prefix_label'] . ts('Is deceased'),
+        'title' => $options['prefix_label'] . ts('Is deceased / closed'),
         'name' => 'is_deceased',
         'type' => CRM_Utils_Type::T_BOOLEAN,
         'is_fields' => FALSE,
