652b06cbca52f880340cfda62c18128a85f66e98
Stephen Palmstrom <stephen.palmstrom@missionassist.org.uk>
2026-04-02 12:53:04 +0100

    v6.13.0

--- code/CRM/Report/Form/Activity.php
+++ code/CRM/Report/Form/Activity.php
@@ -1018,7 +1018,7 @@ GROUP BY civicrm_activity_id $having {$this->_orderBy}";
           $link = [];
           if ($viewLinks) {
             foreach ($assigneeContactIds as $id => $value) {
-              if (isset($value) && isset($assigneeNames[$id])) {
+              if (isset($value, $assigneeNames[$id])) {
                 $url = CRM_Utils_System::url('civicrm/contact/view',
                   'reset=1&cid=' . $value,
                   $this->_absoluteUrl
@@ -1098,7 +1098,7 @@ GROUP BY civicrm_activity_id $having {$this->_orderBy}";
           $link = [];
           if ($viewLinks) {
             foreach ($targetContactIds as $id => $value) {
-              if (isset($value) && isset($targetNames[$id])) {
+              if (isset($value, $targetNames[$id])) {
                 $url = CRM_Utils_System::url("civicrm/contact/view",
                   'reset=1&cid=' . $value,
                   $this->_absoluteUrl
